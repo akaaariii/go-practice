a8ec1e30d87b097ffae67e7dd11fa65fbf89759f
akaaariii <caaari23@gmail.com>
2021-06-30 21:31:02 -0700

    hello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/akaaariii/go-practice
+
+go 1.16
---
+++ hello/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	Hello()
+	fmt.Println("This is my first Go project!")
+}
+
+func Hello() {
+	fmt.Println("Hello World!")
+}
